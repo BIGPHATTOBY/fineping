54e835658a10d03ac911d49afe2b0aa6e1d05b18
Toby <toby@toby.dk>
2017-11-20 20:04:50 +0100

    Added some console style with string formatting

--- fineping.py
+++ fineping.py
@@ -41,9 +41,22 @@ def ping(host, desc):
 
 def update():
     """ Updating state to host """
+    longest_ip = 0
+    longest_desc = 0
+    for hosts in HOSTS_ARR:
+        if len(hosts[0]) > longest_ip:
+            longest_ip = len(hosts[0])
+        if len(hosts[1]) > longest_desc:
+            longest_desc = len(hosts[1])
+
     while True:
+        pre_string = '{0:<' + str(longest_desc) + 's} | {1:' + str(longest_ip) + 's} | {2:15s}'
+        print(pre_string.format('DESC.', 'IP', 'STATE'))
+        pre_string = '{0:-^' + str(longest_desc + longest_ip + 18) + 's}'
+        print(pre_string.format(''))
         for hosts in HOSTS_ARR:
-            print('Hosten: ' + hosts[1] + ' - ' + hosts[0] + ' | ' + hosts[2])
+            pre_string = '{0:<' + str(longest_desc) + 's} | {1:' + str(longest_ip) + 's} | {2:15s}'
+            print(pre_string.format(hosts[1], hosts[0], hosts[2]))
         time.sleep(5)
         subprocess.call('clear')
 
@@ -54,30 +67,32 @@ def update():
 
 ### RUNNING CODE ####
 
-if sys.argv[1] == '-s':
+if len(sys.argv) == 1 or sys.argv[1] == '-h':
+    print('-h | shows this')
+    print('-s | manual host entry, sample syntax: 8.8.8.8,GoogleDNS')
+    print('-S | mulitple manual host entry, sample syntax: ', end='')
+    print('8.8.8.8,Google DNS,8.8.4.4,GoogleDNSBackup')
+    print('-l | entry from list (file), same syntax as -s with newline as divider')
+    print('Example: python3 fineping.py -S 8.8.8.8,Google,8.8.4.4,GoogleBackup')
+
+elif sys.argv[1] == '-s':
     while True:
         _thread.start_new_thread(ping, (str(sys.argv[2].split(',')[0]), sys.argv[2].split(',')[1],))
         time.sleep(2)
         subprocess.call('clear')
 
 
-if sys.argv[1] == '-S':
+elif sys.argv[1] == '-S':
     ARGV_ARR = split_second_comma(sys.argv[2])
     for items in ARGV_ARR:
         HOSTS_ARR.append([items.split(',')[0], items.split(',')[1], 'init'])
     update()
 
 
-if sys.argv[1] == '-l':
+elif sys.argv[1] == '-l':
     f = open(sys.argv[2], 'r')
     for lines in f:
         formattedLine = lines.split()[0]
         HOSTS_ARR.append([formattedLine.split(',')[0], formattedLine.split(',')[1], 'init'])
     update()
 
-elif len(sys.argv) == 1 or sys.argv[1] == '-h':
-    print('-h | shows this')
-    print('-s | manual host entry, sample syntax: 8.8.8.8,GoogleDNS')
-    print('-S | mulitple manual host entry, sample syntax: ', end='')
-    print('8.8.8.8,Google DNS,8.8.4.4,GoogleDNSBackup')
-    print('-l | entry from list (file), same syntax as -s with newline as divider')
